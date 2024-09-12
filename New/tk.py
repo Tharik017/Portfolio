556d18e4ba65546a826b6f7fd8f79e87e89e8042
Tharik017 <146923304+Tharik017@users.noreply.github.com>
2024-09-12 05:55:55 +0530

    Update tk.py

--- New/tk.py
+++ New/tk.py
@@ -74,17 +74,17 @@ if selected=='About':
                      Graduate with an aptitude for aptitude and passion for Continuous Learning.
                      Eager to contribute to collaborative and impactful Software development projects and drive innovation in field""")
             st.subheader("Resume")
-            file_path = 'Mohamed Tharik.PDF'
-            def get_file(file_path):
-                with open(file_path, 'rb') as file:
+            file_path4 = "Mohamed Tharik.pdf"
+            def get_file(file_path4):
+                with open(file_path4, 'rb') as file:
                     return file.read()
 
             def main():
-                st.title("File Download Example")
-                if os.path.exists(file_path):
+                st.title("File Download")
+                if os.path.exists(file_path4):
                     st.write("Click the button below to download the file:")
                     # Get the file data
-                    file_data = get_file(file_path)
+                    file_data = get_file(file_path4)
                     # Create a download button
                     st.download_button(
                     label="Download PDF",
