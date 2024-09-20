b0d3433ff2c6415bd6bfc109e5c3afef69a60f5d
Tharik017 <146923304+Tharik017@users.noreply.github.com>
2024-09-21 01:51:41 +0530

    Update tk.py

--- New/tk.py
+++ New/tk.py
@@ -180,7 +180,7 @@ if selected=='Projects':
                  The project entitled “Plant Disease Identification” is crucial for ensuring plant quality and food security.
                  Recent advances in computer vision and machine learning techniques have made it possible 
                  to accurately detect, classify, and monitor plant diseases based on images of leaves.""")
-            st.markdown("[Contact me...](dm.wa.link/e4r2ws)")
+            st.markdown("[Contact me...](https://wa.link/ak766u)")
             st.write("#####")
             st.write("---")
             st.write("#####")
@@ -200,7 +200,7 @@ if selected=='Projects':
                  Shop Management System is a software application to be developed to manage most of the activities or tasks running in a grocery shop.
                  This system divided in to three main categories: cashier/stock management, supplier information management/generating report, staff management00.
                  This application will provide the basic features such as cashier to handle sales transaction, stock management to control.""")
-            st.markdown("[Contact me...](dm.wa.link/e4r2ws)")
+            st.markdown("[Contact me...](https://wa.link/ak766u)")
             st.write("#####")
             st.write("---")
             st.write("#####")
@@ -224,7 +224,7 @@ if selected=='Projects':
                   This application is built such a way that it should suits for all type of Eye Banks in future. 
                   So every effort is taken to implement this project in this Eye Bank, on successful implementation in this Eye Bank, 
                   we can target other Eye Banks in the city""")
-            st.markdown("[Contact me...](dm.wa.link/e4r2ws)")
+            st.markdown("[Contact me...](https://wa.link/ak766u)")
             st.write("###")
             st.write("---")
 
